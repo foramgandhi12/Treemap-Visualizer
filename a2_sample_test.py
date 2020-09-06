58541cc7a06b3dceea66bc94fe76d81abe1e1ec3
Foram <Foramgandhi12@yahoo.ca>
2020-09-06 15:55:23 -0400

    Update a2_sample_test.py

--- a2_sample_test.py
+++ a2_sample_test.py
@@ -1,41 +1,3 @@
-"""Assignment 2 - Tests
-
-=== CSC148 Winter 2019 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2019 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-
-=== Module Description ===
-This module contains sample tests for Assignment 2, Tasks 1 and 2.
-The tests use the provided example-directory, so make sure you have downloaded
-and extracted it into the same place as this test file.
-This test suite is very small. You should plan to add to it significantly to
-thoroughly test your code.
-
-IMPORTANT NOTES:
-    - If using PyCharm, go into your Settings window, and go to
-      Editor -> General.
-      Make sure the "Ensure line feed at file end on Save" is NOT checked.
-      Then, make sure none of the example files have a blank line at the end.
-      (If they do, the data size will be off.)
-
-    - os.listdir behaves differently on different
-      operating systems.  These tests expect the outcomes that one gets
-      when running on the *Teaching Lab machines*.
-      Please run all of your tests there - otherwise,
-      you might get inaccurate test failures!
-
-    - Depending on your operating system or other system settings, you
-      may end up with other files in your example-directory that will cause
-      inaccurate test failures. That will not happen on the Teachin Lab
-      machines.  This is a second reason why you should run this test module
-      there.
-"""
 import os
 
 from hypothesis import given
@@ -97,27 +59,18 @@ def test_single_file_rectangles(x, y, width, height) -> None:
 def test_example_data_rectangles() -> None:
     """This test sorts the subtrees, because different operating systems have
     different behaviours with os.listdir.
-
-    You should *NOT* do any sorting in your own code
     """
     tree = FileSystemTree(EXAMPLE_PATH)
     _sort_subtrees(tree)
 
     tree.update_rectangles((0, 0, 200, 100))
     rects = tree.get_rectangles()
-
-    # IMPORTANT: This test should pass when you have completed Task 2, but
-    # will fail once you have completed Task 5.
-    # You should edit it as you make progress through the tasks,
-    # and add further tests for the later task functionality.
     assert len(rects) == 6
 
     # UPDATED:
     # Here, we illustrate the correct order of the returned rectangles.
     # Note that this corresponds to the folder contents always being
-    # sorted in alphabetical order. This is enforced in these sample tests
-    # only so that you can run them on your own comptuer, rather than on
-    # the Teaching Labs.
+    # sorted in alphabetical order. 
     actual_rects = [r[0] for r in rects]
     expected_rects = [(0, 0, 94, 2), (0, 2, 94, 28), (0, 30, 94, 70),
                       (94, 0, 76, 100), (170, 0, 30, 72), (170, 72, 30, 28)]
@@ -144,9 +97,8 @@ def is_valid_colour(colour: Tuple[int, int, int]) -> bool:
 
 def _sort_subtrees(tree: TMTree) -> None:
     """Sort the subtrees of <tree> in alphabetical order.
-    THIS IS FOR THE PURPOSES OF THE SAMPLE TEST ONLY; YOU SHOULD NOT SORT
-    YOUR SUBTREES IN THIS WAY. This allows the sample test to run on different
-    operating systems.
+    THIS IS FOR THE PURPOSES OF THE SAMPLE TEST ONLY; This allows the sample 
+    test to run on different operating systems.
 
     This is recursive, and affects all levels of the tree.
     """
