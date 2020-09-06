665017bd36079ffbb099236af312d662e94cab74
Foram <Foramgandhi12@yahoo.ca>
2020-09-06 15:59:05 -0400

    Update print_dirs.py

--- print_dirs.py
+++ print_dirs.py
@@ -1,23 +1,10 @@
-"""Assignment 2: Demonstration of using the os module
-
-=== CSC148 Winter 2019 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2019 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-"""
-
 import os
 
 
 def print_items(d: str, indentation: str) -> None:
     """A sample program showing how to recurse on directories using os.
 
-    Print the list of files and directories in directory <d>, recursively,
+    Prints the list of files and directories in directory <d>, recursively,
     prefixing each with the given <indentation>.
     """
     print(indentation + d + ':')
